9973e3197bb6d0c5825dce2b4cecff46e168fc29
asa1984 <satoasa9913@gmail.com>
2024-10-29 16:25:41 +0900

    既存コンポーネントをMUIに置き換え

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Typography,
+} from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
+    <Container maxWidth="sm">
+      <Box sx={{ my: 4 }}>
+        <Box sx={{ display: "flex", justifyContent: "center", gap: 16 }}>
+          <a href="https://vite.dev" target="_blank">
+            <Box
+              component="img"
+              src={viteLogo}
+              alt="Vite logo"
+              sx={{ height: 100, width: 100 }}
+            />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <Box
+              component="img"
+              src={reactLogo}
+              alt="React logo"
+              sx={{ height: 100, width: 100 }}
+            />
+          </a>
+        </Box>
+        <Typography variant="h1">Vite + React</Typography>
+        <Card>
+          <CardContent>
+            <CardActions sx={{ display: "flex", justifyContent: "center" }}>
+              <Button
+                onClick={() => setCount((count) => count + 1)}
+                variant="contained"
+              >
+                count is {count}
+              </Button>
+            </CardActions>
+            <Typography align="center">
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+      <Typography align="center">
         Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </Typography>
+    </Container>
+  );
 }
 
-export default App
+export default App;
---
+++ src/shared/styles/theme.ts
@@ -0,0 +1,8 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  cssVariables: true,
+  colorSchemes: { dark: true },
+});
+
+export default theme;
